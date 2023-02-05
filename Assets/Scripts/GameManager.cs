52dfae87b281c240c61d0231762b8851ffece053
Zuvuyan <66560381+zuvuyan@users.noreply.github.com>
2023-02-05 17:53:18 +0000

    Code prototype mouse behaviour code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isGameActive = true;
+    public bool infoBoxActive;
+    public TextMeshProUGUI infoText;
+    public GameObject titleScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +21,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameActive)
+        {
+            // code here
+
+        }
     }
+
+    private void OnMouseDown()
+    {
+        if (isGameActive && infoBoxActive)
+        {
+            // Reset the Pillar and clear info Box
+            Debug.Log("Clear info Box");
+        }
+    }
+
 
     public void ExitGame()
     {
--- Assets/Scripts/PillarControl.cs
+++ Assets/Scripts/PillarControl.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PillarControl : MonoBehaviour
 {
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Canvas").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+           
+            gameManager.infoText.text = "text";
+            // activate info Box for Pillar
+            Debug.Log("Pillar Activated");
+        }
     }
 }
